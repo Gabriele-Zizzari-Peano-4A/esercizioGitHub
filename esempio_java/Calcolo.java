1ac7ae2d8b849ce2a29e59386c50b14c6a313ced
Gabriele-Zizzari-Peano-4A <56470876+Gabriele-Zizzari-Peano-4A@users.noreply.github.com>
2019-10-19 09:45:44 +0200

    version 1.0
    version 1.0

---
+++ esempio_java/Calcolo.java
@@ -0,0 +1,33 @@
+package calcolo;
+
+import java.util.Scanner;
+
+public class Calcolo {
+
+    public static void main(String[] args) {
+        int b, h, area, perimetro;
+        Scanner tastiera = new Scanner(System.in);
+
+        System.out.print("Inserisci la base: ");
+        b = tastiera.nextInt();
+
+        System.out.print("Inserisci l'altezza: ");
+        h = tastiera.nextInt();
+
+        System.out.print("Area: " + calcolo(b, h, 'a'));
+        System.out.print("Perimetro: " + calcolo(b, h, 'p'));
+
+    }
+
+    private static int calcolo(int b, int h, char c) {
+        switch (c) {
+            case 'p':
+                return 2 * (h + b);
+            case 'a':
+                return b * h;
+            default:
+                return 0;
+        }
+    }
+
+}
